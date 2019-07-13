8a97e807299088d10507b0e6be5c169e42149a51
SOFTWARE\achroscielewska <agnieszka.chroscielewska@altkom.pl>
2019-07-13 21:20:12 +0200

    api connected to db

--- bookshelf-api/src/app/app.js
+++ bookshelf-api/src/app/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
- 
+
+const environment = require('../environments/environment');
+const mongoose = require('mongoose');
+
+const URL_DB = environment.db_url;
+
 const app = express();
- 
+
+// connection to mongoDB
+mongoose
+    .connect(URL_DB, { useNewUrlParser: true })
+    .then(() => { console.log("Connected to database!"); })
+    .catch(() => { console.log("Connection to mongoDB failed!"); });
+
 // CORS configuration https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,8 +21,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
     next();
 })
- 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 module.exports = app;
---
+++ bookshelf-api/src/environments/environment.js
@@ -0,0 +1,7 @@
+const environment = {
+  production: false,
+  db_url: "mongodb://localhost:27017/bookshelf",
+  version: "0.1"
+};
+
+module.exports = environment;
