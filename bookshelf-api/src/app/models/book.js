12f98cfff541cababc9020904b5d61b644dedb17
SOFTWARE\achroscielewska <agnieszka.chroscielewska@altkom.pl>
2019-07-13 22:07:22 +0200

    book model added

---
+++ bookshelf-api/src/app/models/book.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    title: { type: String, require: true },
+    description: { type: String, require:  true },
+    bookshelfNo: { type: String, require:  true }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
